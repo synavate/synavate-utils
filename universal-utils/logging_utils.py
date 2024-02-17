a737e6c3c7233024c0825a81d6f9107972fb1559
snyata <p4rlx-news@pm.me>
2024-02-18 03:59:07 +1030

    additional packages

--- universal-utils/logging_utils.py
+++ universal-utils/logging_utils.py
@@ -1,23 +1,33 @@
+import os
+import sys
 import logging
 import ecs_logging
 import sys
 from ecs_logging import StdlibFormatter
 from datetime import datetime
+import uuid
 
+
+
+def setup_logging(log_file="../logs/f"{__name__}-)
 # Get the Logger
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.ERROR)
+_logger = logging.getLogger(__name__)
+_logger.setLevel(logging.ERROR)
 
 # Add an ECS formatter to the Handler
 handler = logging.StreamHandler()
-handler_file = logging.FileHandler(f"../logs/{logger}-{datetime.now()}.log")
+handler_file = logging.FileHandler(f"../logs/{_logger}-{datetime.now()}.log")
 handler.setFormatter(ecs_logging.StdlibFormatter())
 handler_file.setFormatter(ecs_logging.StdlibFormatter())
-logger.addHandler(handler)
-logger.addHandler(handler_file)
+_logger.addHandler(handler)
+_logger.addHandler(handler_file)
+
+def get_logger(name=__name__):
+    """Get a logger instance."""
+    return logging.getLogger(__name__)
 
 # Emit a log!
-logger.debug("Example message!", extra={"http.request.method": "get"})
+_logger.debug("Example message!", extra={"http.request.method": "get"})
 
 
 
